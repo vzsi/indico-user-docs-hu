9345f95694a6dd54e56e173c06bf1677d095d9bd
Varga Zsolt Iván <varga.zsolt.ivan@uni-nke.hu>
2024-03-14 10:13:08 +0100

    plausible

---
+++ docs/javascripts/plausible.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.defer = true;
+script.setAttribute('data-domain', 'learn-hu.indico.vargadigital.com');
+script.src = 'https://plausible.vargadigital.com/js/script.js';
+document.head.appendChild(script);
